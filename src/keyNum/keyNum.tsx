a19cd005cd89cc6a47558e25ab8e5da5b412290c
Marc Agen <marc.agen@epitech.eu>
2026-02-04 16:33:16 +0100

    [ADD]: adding key number component

---
+++ src/keyNum/keyNum.tsx
@@ -0,0 +1,22 @@
+const keyNum = () => {
+    <div>
+        <div style={{display: "flex", left: 0}}>
+            <p>17</p>
+            <p>événements d’ampleur régionale ou plus</p>
+        </div>
+        <div style={{display: "flex", right: 0}}>
+            <p>tournois hebdomadaires</p>
+            <p>140</p>
+        </div>
+        <div style={{display: "flex", left: 0}}>
+            <p>416</p>
+            <p>participants</p>
+        </div>
+        <div style={{display: "flex", right: 0}}>
+            <p>lieux d’accueil d’événements</p>
+            <p>4</p>
+        </div>
+    </div>
+}
+
+export default keyNum;
